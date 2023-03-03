daad5a57f3ea0c2544461bbba05dd1e300d203da
Aditya Sharma <aditya.sharma.adbu@gmail.com>
2023-03-03 11:53:47 +0530

    created user dto

--- src/main/java/codebenchers006/ecommerce/controller/UserController.java
+++ src/main/java/codebenchers006/ecommerce/controller/UserController.java
@@ -4,6 +4,10 @@ import codebenchers006.ecommerce.dto.ResponseDto;
 import codebenchers006.ecommerce.dto.SignInDtoResponse;
 import codebenchers006.ecommerce.dto.user.SignInDto;
 import codebenchers006.ecommerce.dto.user.SignUpDto;
+import codebenchers006.ecommerce.dto.user.UserDto;
+import codebenchers006.ecommerce.model.AuthenticationToken;
+import codebenchers006.ecommerce.model.User;
+import codebenchers006.ecommerce.service.AuthenticationService;
 import codebenchers006.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +20,9 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AuthenticationService authenticationService;
+
     //SignUp
 
     @PostMapping("/signup")
@@ -27,5 +34,19 @@ public class UserController {
     @PostMapping("/signin")
     public SignInDtoResponse singInUser(@RequestBody SignInDto signInDto){
         return userService.signInUser(signInDto);
+    }
+
+
+    @GetMapping("/{token}")
+    public UserDto getUserData(@PathVariable String token){
+
+        AuthenticationToken authenticationToken = authenticationService.getAuthenticationData(token);
+        User user = authenticationToken.getUser();
+        UserDto userDto = UserDto.builder()
+                .id(user.getUser_id())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+        return userDto;
     }
 }
